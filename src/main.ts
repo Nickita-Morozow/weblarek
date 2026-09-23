8dcf98988c09aa4056b0bdf77cd5394916bfdbe2
Морозов Никита Ильич <morozov@office.alfint.ru>
2026-09-23 12:34:19 +0300

    test data models

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,30 @@ const commApi = new CommApi(api);
 commApi.getProducts().then((data) => {
   products.setItems(data.items);
   console.log(products.getItems());
+  const firstProduct = data.items[0];
+  console.log(products.getItem(firstProduct.id));
+  products.setItem(firstProduct);
+  console.log(products.getCurItem());
+  basket.addToBasket(firstProduct);
+  console.log(basket.getItems());
+  console.log(basket.isInBasket(firstProduct.id));
+  console.log(basket.getItemsNumber());
+  console.log(basket.getTotalPrice());
+  basket.removeFromBasket(firstProduct);
+  console.log(basket.getItems());
+  basket.addToBasket(firstProduct);
+  basket.clearBasket();
+  console.log(basket.getItems());
+  console.log(buyer.getData());
+  buyer.setData({ payment: "card" });
+  console.log(buyer.getData());
+  buyer.setData({
+    address: "Moscow",
+    phone: "+7(999)765-92-22",
+    email: "test@test.ru",
+  });
+  console.log(buyer.getData());
+  console.log(buyer.validateData());
+  buyer.clearData();
+  console.log(buyer.validateData());
 });
-console.log(basket.getItems());
-console.log(buyer.getData());
